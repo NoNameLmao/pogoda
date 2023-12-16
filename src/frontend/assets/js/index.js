48382e6de6a63df236100d4127430d5c26abe00e
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2023-12-16 11:29:12 +0000

    mysql фронтенд
    Co-authored-by: BendikaVils3245 <BendikaVils3245@users.noreply.github.com>

--- src/frontend/assets/js/index.js
+++ src/frontend/assets/js/index.js
@@ -1,2 +1,22 @@
 import "../styles/reset.scss";
 import "../styles/styles.scss";
+
+const searchBar = document.getElementsByClassName('search__input')[0]
+const suggestionList = document.getElementById('suggestion-list');
+
+searchBar.addEventListener('input', function () {
+    const query = searchBar.value;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `/cityautocomplete?query=${query}`, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const suggestions = JSON.parse(xhr.responseText);
+            suggestions.forEach(function(suggestion) {
+                const li = document.createElement('li');
+                li.textContent = suggestion;
+                suggestionList.appendChild(li);
+            })
+        }
+    }
+    xhr.send();
+})
